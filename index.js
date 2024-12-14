4d382e8996b0a0ddeeb1b3e5cc68b292ae5dc885
JosephRoust0m <joseph03.roustom@gmail.com>
2024-12-13 22:24:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ app.use(passport.session());
 
 
 const db = new pg.Client({
-  user: "investmeter_iidv_user",
-  host: "dpg-csm46aogph6c73aci6ug-a.oregon-postgres.render.com",
-  database: "investmeter_iidv",
+  user: "investmeter_vd11_user",
+  host: "dpg-ctef9n9u0jms739alhj0-a.oregon-postgres.render.com",
+  database: "investmeter_vd11",
   password: process.env.password,
   port: 5432,
   ssl: {
